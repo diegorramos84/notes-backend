1aa01ef563e9f50b3f80ccf7d809c181c077d9ba
Diego Ramos <diegorramos84@gmail.com>
2022-08-31 19:50:37 +0100

    add validations

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.put('/api/notes/:id' , (request, response, next) => {
 })
 
 
-app.post('/api/notes' , (request, response) => {
+app.post('/api/notes' , (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -81,9 +81,11 @@ app.post('/api/notes' , (request, response) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response, next) => {
@@ -98,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send( { error: 'malformatted id'} )
+  } else if (error.name == 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
