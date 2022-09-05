adaa25a961eec1ebd94e194192d415b6bf22624f
Diego Ramos <diegorramos84@gmail.com>
2022-09-05 15:42:36 +0100

    add tests

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -17,3 +17,9 @@ test('reverse of releveler', () => {
 
   expect(result).toBe('releveler')
 })
+
+test('palindrome of react', () => {
+  const result = reverse('react')
+
+  expect(result).toBe('tkaer')
+})
