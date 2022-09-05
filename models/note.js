182089a7612214d1e94ae7a0ebd92bbe00c2c951
Diego Ramos <diegorramos84@gmail.com>
2022-09-05 15:32:37 +0100

    add jest

--- models/note.js
+++ models/note.js
@@ -1,26 +1,14 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connection to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MondoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
-  date:{
+  date: {
     type: Date,
-    required: true
+    required: true,
   },
   important: Boolean,
 })
